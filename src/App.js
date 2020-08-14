9b62b9ee219202515d55e22ed31db6e5a94e0078
Leida_Gandy <leida.gandy@gmail.com>
2020-08-14 18:44:55 +0100

    Fetches data from server on Glich and allow user to get a random quote from the server by clicking a button

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import DisplayQuote from "./DisplayQuote";
+
+function App() {
+  return (
+
+    <div className="App">
+    <p></p>
+      <div>
+        <DisplayQuote />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/DisplayQuote.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+
+export default function DisplayQuote() {
+  const [quotes, setQuotes] = useState();
+  const [nextQuote, setNextQuote] = useState();
+
+  useEffect(() => {
+    fetch(`https://leida-quote-server.glitch.me/quotes`)
+      .then((response) => response.json())
+      .then((data) => setQuotes(data));
+  }, []);
+
+  function pickFromArray(quotesArr) {
+    return quotesArr[Math.floor(Math.random() * quotesArr.length)];
+  }
+
+  function pickNextQuote() {
+    setNextQuote(pickFromArray(quotes));
+  }
+
+  if (!quotes) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <div class="container">
+        <blockquote>
+          {nextQuote ? nextQuote.quote : pickFromArray(quotes).quote}
+        </blockquote>
+        <cite>
+          {nextQuote ? nextQuote.author : pickFromArray(quotes).author}
+        </cite>
+        <div className="btn-div">
+          <button className="btn" onClick={pickNextQuote}>
+            Show a new quote
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
